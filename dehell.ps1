7450068bac26321062dee2204cd57af9ee6fb0ed
Mikael Porttila <mikael.porttila@gmail.com>
2020-09-12 21:49:24 +0200

    Format

--- dehell.ps1
+++ dehell.ps1
@@ -2,81 +2,73 @@
     Author: Mikael Porttila
 #>
 
-function Is-Repo
-{
-    param($path)
+function Is-Repo {
+    param (
+        [Parameter(Mandatory=$true)] [String]$path
+    )
     return (-not (git -C $path rev-parse))
 }
 
-$path = $args[0]
-$useConfig = $false
+function Git-Update {
+    param (
+        [Parameter(Mandatory=$false)] [String]$path
+    )
 
-if($path -like "*.json")
-{
-    $useConfig = $true
-}
-else
-{
-    if([string]::IsNullOrEmpty($path))
-    {
-        $path = Get-Location
-    }
-    
-    $configPath = Join-Path $path 'dehell.json';
-
-    if(Test-Path $configPath)
-    {
+    if ($path -like "*.json") {
         $useConfig = $true
-        $path = $configPath
-    }
-}
-
-$repositories = @()
-$dir
-if($useConfig) 
-{
-    # Load config
-    $path = Resolve-Path $path
-    $dir = Split-Path -Path $path
-    $repositories = (Resolve-Path $path | Get-Content | ConvertFrom-Json).repositories;
-}
-else 
-{
-    # Scan for git repositories
-    $dir = Resolve-Path $path;
-    Get-ChildItem -Directory -Path $dir | Select-Object -property name | ForEach-Object {
-        if(Is-Repo($_.name)) 
-        {
-            $repositories += $_.name;
+    } else {
+        if ([string]::IsNullOrEmpty($path)) {
+            $path = Get-Location
         }
-    } 
-}
+        
+        $configPath = Join-Path $path 'dehell.json';
 
-$repositories | where {Is-Repo($_)} | ForEach-Object -Parallel {
-    $repoName = $_;
-    $repoPath = Join-Path $using:dir $repoName | Resolve-Path;
-
-    Write-Host "[$repoName] Sync" -ForegroundColor DarkGreen
-
-    # Stash work in progress if needed
-    $skipStash = -not (git -C $repoPath status --short --untracked-files=no);
-    if(!$skipStash) 
-    {
-        Write-Host "[$repoName] Stashing" -ForegroundColor DarkGreen
-        git -C $repoPath stash -q;
+        if (Test-Path $configPath) {
+            $useConfig = $true
+            $path = $configPath
+        }
     }
 
-    # Fetch and Pull
-    git -C $repoPath fetch -q;
-    git -C $repoPath pull -q;
-
-    # Pop stash if needed
-    if(!$skipStash) 
-    {
-        git -C $repoPath stash pop -q;
-        Write-Host "[$repoName] Stash popped" -ForegroundColor DarkGreen
+    $repositories = @()
+    if ($useConfig) {
+        $path = Resolve-Path $path
+        $dir = Split-Path -Path $path
+        $repositories = (Resolve-Path $path | Get-Content | ConvertFrom-Json).repositories;
     }
-    
-    Write-Host "[$repoName] Done ✔" -ForegroundColor DarkGreen
-    
-} -ThrottleLimit 3;
+    else {
+        $dir = Resolve-Path $path;
+        Get-ChildItem -Directory -Path $dir | Select-Object -property name | ForEach-Object {
+            if (Is-Repo($_.name)) {
+                $repositories += $_.name;
+            }
+        } 
+    }
+
+    $repositories | where {Is-Repo($_)} | ForEach-Object -Parallel {
+        $repoName = $_;
+        $repoPath = Join-Path $using:dir $repoName | Resolve-Path;
+
+        Write-Host "[$repoName] Sync" -ForegroundColor DarkGreen
+
+        # Stash work in progress if needed
+        $skipStash = -not (git -C $repoPath status --short --untracked-files=no);
+        if (!$skipStash) {
+            Write-Host "[$repoName] Stashing" -ForegroundColor DarkGreen
+            git -C $repoPath stash -q;
+        }
+
+        # Fetch and Pull
+        git -C $repoPath fetch -q;
+        git -C $repoPath pull -q;
+
+        # Pop stash if needed
+        if (!$skipStash) {
+            git -C $repoPath stash pop -q;
+            Write-Host "[$repoName] Stash popped" -ForegroundColor DarkGreen
+        }
+        
+        Write-Host "[$repoName] Done ✔" -ForegroundColor DarkGreen
+    } -ThrottleLimit 3;
+} 
+
+Git-Update($args[0])
