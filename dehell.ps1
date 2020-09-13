dc17c6b73b08675bac7e22a05093f963db307684
Mikael Porttila <mikael.porttila@gmail.com>
2020-09-13 18:45:28 +0200

    Work in progress

--- dehell.ps1
+++ dehell.ps1
@@ -9,9 +9,25 @@ function Is-Repo {
     return (-not (git -C $path rev-parse))
 }
 
-function Git-Update {
+# Credit: https://stackoverflow.com/users/602585/deadlydog
+function Get-DevEnvExecutableFilePath
+{
+    [bool] $vsSetupExists = $null -ne (Get-Command Get-VSSetupInstance -ErrorAction SilentlyContinue)
+    if (!$vsSetupExists)
+    {
+        Write-Verbose "Installing the VSSetup module..."
+        Install-Module VSSetup -Scope CurrentUser -Force
+    }
+    [string] $visualStudioInstallationPath = (Get-VSSetupInstance | Select-VSSetupInstance -Latest -Require Microsoft.Component.MSBuild).InstallationPath
+
+    $devEnvExecutableFilePath = (Get-ChildItem $visualStudioInstallationPath -Recurse -Filter "DevEnv.exe" | Select-Object -First 1).FullName
+    return $devEnvExecutableFilePath
+}
+
+function Refresh-And-Run {
     param (
-        [Parameter(Mandatory=$false)] [String]$path
+        [Parameter(Mandatory=$false)] $path,
+        [Parameter(Mandatory=$false)] [bool]$runDebug = $false
     )
 
     if ($path -like "*.json") {
@@ -29,22 +45,22 @@ function Git-Update {
         }
     }
 
-    $repositories = @()
+    $repos = @()
     if ($useConfig) {
         $path = Resolve-Path $path
         $dir = Split-Path -Path $path
-        $repositories = (Resolve-Path $path | Get-Content | ConvertFrom-Json).repositories;
+        $repos = (Resolve-Path $path | Get-Content | ConvertFrom-Json).repositories;
     }
     else {
         $dir = Resolve-Path $path;
         Get-ChildItem -Directory -Path $dir | Select-Object -property name | ForEach-Object {
             if (Is-Repo($_.name)) {
-                $repositories += $_.name;
+                $repos += $_.name;
             }
         } 
     }
 
-    $repositories | where {Is-Repo($_)} | ForEach-Object -Parallel {
+    $repos | where {Is-Repo($_)} | ForEach-Object -Parallel {
         $repoName = $_;
         $repoPath = Join-Path $using:dir $repoName | Resolve-Path;
 
@@ -69,6 +85,63 @@ function Git-Update {
         
         Write-Host "[$repoName] Done ✔" -ForegroundColor DarkGreen
     } -ThrottleLimit 3;
+
+    if($runDebug) {
+        $repos | ForEach-Object {
+            $repo = $_
+            $repoPath = Join-Path $dir $repo | Resolve-Path;
+            $files = Get-ChildItem -Path $repoPath -File;
+
+            $dotnetSolutions = $files | where { $_ -like '*.sln'}
+            if(($dotnetSolutions).count -gt 0) {
+                $devEnv = Get-DevEnvExecutableFilePath
+                if($devEnv) {
+                    # https://docs.microsoft.com/en-us/visualstudio/ide/reference/devenv-command-line-switches?view=vs-2019
+                    write-Host "[$repo] devenv /R" -ForegroundColor DarkBlue
+                }
+                else {
+                    write-Host "[$repo] Missing devenv" -ForegroundColor DarkRed
+                }
+            }
+
+            $dotnetProjects = $files | where { $_ -like '*.csproj'}
+            if(($dotnetProjects).count -gt 0) {
+                $devEnv = Get-DevEnvExecutableFilePath
+                if($devEnv) {
+                    # https://docs.microsoft.com/en-us/visualstudio/ide/reference/devenv-command-line-switches?view=vs-2019
+                    write-Host "[$repo] devenv /R" -ForegroundColor DarkBlue
+                    Start-Process -FilePath $devEnv /R -WorkingDirectory $repoPath -NoNewWindow false &
+                }
+                else {
+                    write-Host "[$repo] Missing devenv" -ForegroundColor DarkRed
+                }
+                return;
+            }
+
+            $nodeProjects = $files | where { $_ -like '*package.json'}
+            if(($nodeProjects).count -gt 0) {
+                #TODO (MIKAEL) Parse package.json and look for start, dev or debug (and/or what garbage like Angular uses...)
+                write-Host "[$repo] npm start" -ForegroundColor DarkBlue
+                Start-Process -FilePath npm start -WorkingDirectory $repoPath -NoNewWindow false &
+                return;
+            }
+
+            $denoProjects = $files | where { $_ -like '*denon.json'}
+            if(($denoProjects).count -gt 0) {
+                #TODO (MIKAEL) Parse denon file and search for start, dev, debug
+                write-Host "[$repo] denon start" -ForegroundColor DarkBlue
+                Start-Process -FilePath denon start -WorkingDirectory $repoPath -NoNewWindow false &
+                return;
+            }
+
+            
+        }
+    }
 } 
 
-Git-Update($args[0])
+$runDebug = $false;
+if($args.count -gt 1) {
+    $runDebug = $args[1]
+}
+
+Refresh-And-Run $args[0] $runDebug
